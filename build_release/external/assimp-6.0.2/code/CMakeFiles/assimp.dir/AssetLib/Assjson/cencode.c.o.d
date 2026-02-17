b3cbf21612333ccde1447691cd1afb1127cd1179
Κωνσταντίνος Κανελλόπουλος <79727099+BenizelosOEllhn@users.noreply.github.com>
2026-02-17 04:06:09 +0200

    Delete build_release directory

--- build_release/external/assimp-6.0.2/code/CMakeFiles/assimp.dir/AssetLib/Assjson/cencode.c.o.d
+++
@@ -1,3 +0,0 @@
-external/assimp-6.0.2/code/CMakeFiles/assimp.dir/AssetLib/Assjson/cencode.c.o: \
-  /Users/konstantinoskanellopoulos/Desktop/rts/external/assimp-6.0.2/code/AssetLib/Assjson/cencode.c \
-  /Users/konstantinoskanellopoulos/Desktop/rts/external/assimp-6.0.2/code/AssetLib/Assjson/cencode.h
--- build_release/external/assimp-6.0.2/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/konstantinoskanellopoulos/Desktop/rts/external/assimp-6.0.2/contrib/zlib
-# Build directory: /Users/konstantinoskanellopoulos/Desktop/rts/build_release/external/assimp-6.0.2/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
